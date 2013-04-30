139a9a5bf4a6f0c8c0e6b55b71b42820f464739e
Harlow Ward <harlow@thoughtbot.com>
2013-04-30 01:03:27 -0700

    Remove unused files

--- spec/features/user_manages_tasks_spec.rb
+++ spec/features/user_manages_tasks_spec.rb
@@ -9,28 +9,6 @@ feature 'User manages tasks' do
     fill_in 'Name', with: task_name
     click_button('Create Task')
 
-    expect(page).to have_content(task_name)
-  end
-
-  scenario 'view all tasks' do
-    task_name = 'Mow the lawn'
-    Task.create!(name: task_name)
-
-    visit root_path
-
-    expect(page).to have_content(task_name)
-  end
-
-  scenario 'user edits a task' do
-    original_name = 'Mow the lawn'
-    updated_name = 'Paint the fence'
-    Task.create!(name: original_name)
-
-    visit root_path
-    click_link 'Edit'
-    fill_in 'Name', with: updated_name
-    click_button('Update Task')
-
-    expect(page).to have_content(updated_name)
+    expect(page).to have_content task_name
   end
 end
