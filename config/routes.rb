a8dbe3da1c25202cccfdfe1231c6dcd9ff179457
Adarsh Pandit <adarsh@thoughtbot.com>
2013-04-29 22:12:21 -0700

    User completes a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TddIntro::Application.routes.draw do
-  resources :tasks, only: [:index, :new, :create]
+  resources :tasks, only: [:index, :new, :create] do
+    resources :completions, only: [:create]
+  end
+
   root to: 'tasks#index'
 end
---
+++ db/migrate/20130430074038_add_completion_date_to_task.rb
@@ -0,0 +1,5 @@
+class AddCompletionDateToTask < ActiveRecord::Migration
+  def change
+    add_column :tasks, :completed_at, :timestamp
+  end
+end
