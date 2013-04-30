82dfe3409dbba838d47836fecbd0288237cd0428
Harlow Ward <harlow@thoughtbot.com>
2013-04-30 01:13:03 -0700

    Add index test

--- spec/features/user_manages_tasks_spec.rb
+++ spec/features/user_manages_tasks_spec.rb
@@ -7,7 +7,16 @@ feature 'User manages tasks' do
     visit root_path
     click_link 'New Task'
     fill_in 'Name', with: task_name
-    click_button('Create Task')
+    click_button 'Create Task'
+
+    expect(page).to have_content task_name
+  end
+
+  scenario 'view all tasks' do
+    task_name = 'Mow the lawn'
+    Task.create!(name: task_name)
+
+    visit root_path
 
     expect(page).to have_content task_name
   end
